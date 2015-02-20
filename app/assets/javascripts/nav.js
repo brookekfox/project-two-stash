52eccf95c51e088ceece58a6f53fdc25802f254b
brookekfox <brookekfox@gmail.com>
2015-02-20 12:02:47 -0800

    change to jQuery

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,7 +1,11 @@
-$(document).ready(function(){
-	$('.navbar .dropdown').hover(function() {
-		$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-	}, function() {
-		$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-	});
-	n})
+//$(document).ready(function(){
+//	$('.navbar .dropdown').hover(function() {
+//		$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+//	}, function() {
+//		$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+//	});
+//})
+
+$('.dropdown').hover(function(){
+	$('.dropdown-toggle', this).trigger('click');
+});
